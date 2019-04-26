9ea632d65cb6d55157d424bd8d4970456121c12b
Kyle Goff <kyle.a.goff@gmail.com>
2019-04-26 16:00:57 -0700

    Allow simple commands with arguments

--- include/kgbash.h
+++ include/kgbash.h
@@ -1,7 +1,10 @@
 #ifndef KGBASH_H
 #define KGBASH_H
 
+#define MAX_COMMANDS 12
+#define COMMAND_ARRAY_LEN (MAX_COMMANDS + 1)
 #define MAX_INPUT_LENGTH 512
+#define INPUT_ARRAY_LEN (MAX_INPUT_LENGTH + 1)
 #define QUIT_STRING "quit"
 
 #endif // KGBASH_H
--- src/kgbash.c
+++ src/kgbash.c
@@ -1,20 +1,89 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+// #include <sys/wait.h>
 #include "kgbash.h"
 
+// TODO: write this
+bool is_special_token(char token) {
+    return false;
+}
+
+bool is_white_space_or_null(char token) {
+    if(token == ' ' || token == '\t' || token == '\0') {
+        return true;
+    }
+    return false;
+}
+
 /*
- * Returns a string that contains only the null-terminated user input without
- * a trailing endline character.
+ * Assumes cmd input defined as cmd[n_commands][string_size]
  */
- void parse_raw_input(char * string, size_t string_size) {
-    int string_idx = 0;
+bool separate_commands (char** cmd, char* string, size_t n_commands, size_t string_size) {
+
+    // Sanity check
+    if(!cmd || !string) {
+        return false;
+    }
+
+    uint32_t string_idx = 0;
+    uint32_t cmd_idx = 0;
+    uint32_t cmd_string_idx = 0;
+
+    // Gather commands
+    while(cmd_idx < n_commands) {
+        // TODO: free this!!
+        cmd[cmd_idx] = malloc(string_size*sizeof(char));
+
+        // For each command index, parse the string until a special token
+        while(!is_white_space_or_null(string[string_idx]) &&
+              string_idx < string_size && cmd_string_idx < string_size) {
+            cmd[cmd_idx][cmd_string_idx++] = string[string_idx++];
+        }
+
+        // If reached null terminator, stop
+        if(string[string_idx] == '\0') {
+            if(cmd_idx < (n_commands-1)) {
+                cmd[cmd_idx+1] = NULL;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // Skip over white space and keep gathering commands
+        string_idx++;
+        cmd_idx++;
+        cmd_string_idx = 0;
+    }
+    return true;
+}
+
+/*
+ * Fills a string to contain only the null-terminated user input without
+ * a trailing endline character.
+ * 
+ * Returns:
+ *  - False if input is empty
+ *  - True otherwise
+ */
+ bool parse_raw_input(char * string, size_t string_size) {
+    uint32_t string_idx = 0;
 
     // Collect user input
     do {
         string[string_idx] = getchar();
     } while(string[string_idx++] != '\n' &&
             string_idx < string_size);
+
+    // Return false on empty user input
+    if(string[0] == '\n') {
+        return false;
+    }
 
     // Clear input buffer if needed
     if(string[string_idx-1] != '\n') {
@@ -23,23 +92,56 @@
     } else {
         string[string_idx-1] = '\0';
     }
+
+    return true;
 }
 
 int main() {
 
-    char raw_input[MAX_INPUT_LENGTH + 1];
+    char raw_input[INPUT_ARRAY_LEN];
+    char *cmd[COMMAND_ARRAY_LEN];
+    int retval;
+    pid_t pid;
 
     do {
         // Reset user input
-        memset(raw_input, 0, MAX_INPUT_LENGTH + 1);
+        memset(raw_input, 0, INPUT_ARRAY_LEN*sizeof(char));
+        memset(cmd, 0, COMMAND_ARRAY_LEN*sizeof(char*));
 
         // Prompt user for input
-        printf("kgbash: ");
-        parse_raw_input(raw_input, MAX_INPUT_LENGTH + 1);
+        fprintf(stdout, "kgbash: ");
+
+        // Gather user input data, and skip fork if empty
+        if(!parse_raw_input(raw_input, INPUT_ARRAY_LEN)) {
+            fprintf(stdout, "\n");
+            continue;
+        }
+
+        // TODO: separate out the input into distinct command arguments
+        if(!separate_commands((char**)cmd, raw_input, COMMAND_ARRAY_LEN, INPUT_ARRAY_LEN)) {
+            fprintf(stdout, "\nInvalid input: %s\n", raw_input);
+            continue;
+        }
 
         // Display the user's command
-        printf("%s\n", raw_input);
+        fprintf(stdout, "%s\n", cmd[0]);
+
+        pid = fork();
+        if (pid == 0) {
+            execvp(cmd[0], (char**)cmd);
+            exit(EXIT_FAILURE);
+        }
+        else if (pid > 0) {
+            wait(&retval);
+            retval = EXIT_SUCCESS;
+        }
+        else {
+            exit(EXIT_FAILURE);
+        }
+
+        fprintf(stderr, "+ completed '%s %s' [%d]\n", cmd[0], cmd[1], retval);
+
     } while(strncmp(raw_input, QUIT_STRING, sizeof(QUIT_STRING)-1));
 
-    return 0;
+    return EXIT_SUCCESS;
 }
